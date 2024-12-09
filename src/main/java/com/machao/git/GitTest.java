c03af11729d9ec58ab72d9cbe36b84d5a7e5b807
lvegyr <1832348287@qq.com>
2024-12-09 21:25:29 +0800

    master push commit

--- src/main/java/com/machao/git/GitTest.java
+++ src/main/java/com/machao/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("这是hot-fix分支修改的内容");
         System.out.println("这是master分支修改的内容");
         System.out.println("这是hot-fix分支修改的内容");
+        System.out.println("push test");
     }
 }
